21f3923658063c7615aa9af59d3c621f33336f00
Wildhoney <adam.timberlake@gmail.com>
2015-09-16 08:02:31 +0100

    Updated dependencies

--- cattery/project.clj
+++ cattery/project.clj
@@ -1,10 +1,12 @@
 (defproject cattery "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Web-based API that delivers cats straight to your browser via JSON and YAML."
+  :url "https://github.com/Wildhoney/Cattery"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [cheshire "5.5.0"]
+                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler cattery.handler/app}
   :profiles
