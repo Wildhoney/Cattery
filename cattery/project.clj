5f67bd1944ca3d4cf5fcd517288173e35055ebdb
Wildhoney <adam.timberlake@gmail.com>
2015-09-17 10:47:00 +0100

    Added mock endpoints

--- cattery/project.clj
+++ cattery/project.clj
@@ -4,8 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [cheshire "5.5.0"]
+                 [clj-json "0.5.3"]
                  [ring/ring-defaults "0.1.2"]
+                 [ring-json-params "0.1.3"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler cattery.handler/app}
--- cattery/src/cattery/handler.clj
+++ cattery/src/cattery/handler.clj
@@ -1,11 +1,40 @@
 (ns cattery.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+  (:use compojure.core)
+  (:use ring.middleware.json-params)
+  (:require [clj-json.core :as json]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (json/generate-string data)})
+
+;; capitalise-all :: string -> string
+;(defn capitalise-all [name]
+;  (clojure.string/join
+;    (map clojure.string/capitalize
+;      (clojure.string/split (str name) #"\b"))))
+;
+;;; thousand-times :: int -> int
+;(def thousand-times
+;  (partial * 1000))
+;
+;;; random-coin-value :: _ -> int
+;(def random-coin-value
+;  (partial rand-int 10))
+
+(defroutes handler
+;  (GET "/:name" [name]
+;    (let [coins (thousand-times (random-coin-value))]
+;      (json-response {"name" (capitalise-all name),
+;                      "coins_available" coins,
+;                      "is_bankrupt" (zero? coins)})))
+
+  (GET "/random/:count" [count]
+    (json-response [{"url" "http://www.catgifpage.com/gifs/241.gif"}]))
+
+  (GET "/random" []
+    (json-response {"url" "http://www.catgifpage.com/gifs/241.gif"})))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> handler
+    wrap-json-params))
